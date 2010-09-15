50b1782d4b6d800f9b02ffb183e339cd408e3607
Martin Kleppmann <martin@rapportive.com>
2010-09-15 12:06:55 +0100

    Event handler methods should be CamelCase (extend the String prototype to add some methods)

--- contacts/lib/has_event.js
+++ contacts/lib/has_event.js
@@ -1,12 +1,11 @@
 function hasEvent(_public, _protected, event_name) {
     var handlers = [];
-    var capitalized = event_name.charAt(0).toUpperCase() + event_name.slice(1);
 
-    _public['on' + capitalized] = function (handler) {
+    _public['on' + event_name.camelize()] = function (handler) {
         handlers.push(handler);
     };
 
-    _public['trigger' + capitalized] = function () {
+    _public['trigger' + event_name.camelize()] = function () {
         var args = arguments, that = this;
         _(handlers).each(function (handler) {
             handler.apply(that, args);
