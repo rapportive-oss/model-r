f0056f6ed8e681d94f7056c713f60f05f55155f8
Martin Kleppmann <martin@rapportive.com>
2010-09-15 10:37:11 +0100

    Javascript mixin for event handling

---
+++ contacts/lib/has_event.js
@@ -0,0 +1,14 @@
+function hasEvent(_public, _protected, event_name) {
+    var handlers = [];
+    var capitalized = event_name.charAt(0).toUpperCase() + event_name.slice(1);
+
+    _public['on' + capitalized] = function (handler) {
+        handlers.push(handler);
+    };
+
+    _public['trigger' + capitalized] = function () {
+        _(handlers).each(function (handler) {
+            handler();
+        });
+    };
+}
