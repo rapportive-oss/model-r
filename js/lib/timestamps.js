8310588a06139fcfeff8367c576d6c5272dff48a
Lee Mallabone <lee@rapportive.com>
2011-07-31 15:33:48 +0100

    Add lib.timestamps to mix in getters to make date types easier.

---
+++ js/lib/timestamps.js
@@ -0,0 +1,65 @@
+/*jslint nomen: false, onevar: false */
+/*global lib, _ */
+
+// Augments _public to add date & timestamp helper getter properties for the specified declared_attributes.
+// NOTE: Those attributes must be unix timestamps for the getters to function.
+//
+// For example, calling:
+// lib.times(_public, _protected, 'due_at')
+// adds these properties to _public:
+// due_at                 - returns the original (unix seconds timestamp) as an integer
+// due_at_timestamp_milli - returns Javascript-friendly milliseconds-since-the-epoch
+// due_at_date            - returns a Javascript date object
+// due_at_iso8601         - an ISO8601-complaint string, eg. 2011-04-23T0623
+//
+// TODO: should this be automatic for any model attributes with a key ending in _at?
+lib.timestamps = function (_public, _protected, declared_attributes) {
+
+    // Allow either lib.times(_p, _p, ['a','b','c']);
+    //           or lib.times(_p, _p, 'a', 'b', 'c');
+    //
+    // The latter is more pleasant to interact with as a human,
+    // the former is better for automated interaction.
+    if (!_(declared_attributes).isArray()) {
+        declared_attributes = _(arguments).toArray().slice(2);
+    }
+
+    // Add helper getters for each attribute, they chain off each other
+    _(declared_attributes).each(function (key) {
+        var timestamp_method = key + "_timestamp";
+        _public.__defineGetter__(timestamp_method, function () {
+            return parseInt(_public[key], 10);
+        });
+        
+        var timestamp_milli_method = timestamp_method + "_milli";
+        _public.__defineGetter__(timestamp_milli_method, function () {
+            var timestamp = _public[timestamp_method];
+            if (timestamp) {
+                return timestamp * 1000;
+            } else {
+                return null; 
+            }
+        });
+
+        var date_method = key + "_date";
+        _public.__defineGetter__(date_method, function () {
+            var stamp = _public[timestamp_milli_method];
+            if (stamp) {
+                return new Date(stamp);
+            } else {
+                return null;
+            }
+        });
+
+        _public.__defineGetter__(key + "_iso8601", function () {
+            var date = _public[date_method];
+            if (date) {
+                return date.toISOString().replace(/:\d\d\.\d\d\dZ/, "")
+                                         .replace(/:/g, ""); // HACK, emails can haz no colons. still valid iso.
+            } else {
+                return null;
+            }
+        });
+    });
+
+};
---
+++ spec/javascripts/lib/timestamps_spec.js
@@ -0,0 +1,57 @@
+/*jslint nomen: false, onevar: false */
+/*global describe, it, expect, beforeEach, lib, _, jQuery, jasmine */
+
+describe("lib.timestamps", function () {
+    describe("property helpers", function () {
+        function consistentModel() {
+            var _public = {}, _protected = {};
+            lib.model(_public, _protected, "color", "created_at");
+            lib.timestamps(_public, _protected, "created_at");
+            return _public;
+        }
+
+        it("should add helpers for the specified attributes", function () {
+            var model = consistentModel();
+            var frozen_date = new Date(2011, 0, 8, 12, 32); // months are zero-based
+            var frozen_timestamp = parseInt(frozen_date.getTime() / 1000); // unix timestamp
+            model.created_at = frozen_timestamp;
+            
+            expect(model.created_at).toEqual(frozen_timestamp);
+            expect(model.created_at_timestamp).toEqual(frozen_timestamp);
+            expect(model.created_at_timestamp_milli).toEqual(frozen_timestamp * 1000);
+            expect(model.created_at_date).toEqual(frozen_date);
+            expect(model.created_at_iso8601).toEqual('2011-01-08T1232');
+        });
+
+        it("should return latest value after an update", function () {
+            var model = consistentModel();
+            var frozen_date = new Date(2011, 0, 8, 12, 32); // months are zero-based
+            var frozen_timestamp = parseInt(frozen_date.getTime() / 1000); // unix timestamp
+            model.created_at = frozen_timestamp;
+
+            expect(model.created_at).toEqual(frozen_timestamp);
+            expect(model.created_at_timestamp).toEqual(frozen_timestamp);
+            expect(model.created_at_timestamp_milli).toEqual(frozen_timestamp * 1000);
+            expect(model.created_at_date).toEqual(frozen_date);
+            expect(model.created_at_iso8601).toEqual('2011-01-08T1232');
+
+            var second_date = new Date(2012, 1, 2, 12, 32); // months are zero-based
+            var second_stamp = parseInt(second_date.getTime() / 1000);
+            model.created_at = parseInt(second_date.getTime() / 1000);
+
+            expect(model.created_at).toEqual(second_stamp);
+            expect(model.created_at_timestamp).toEqual(second_stamp);
+            expect(model.created_at_timestamp_milli).toEqual(second_stamp * 1000);
+            expect(model.created_at_date).toEqual(second_date);
+            expect(model.created_at_iso8601).toEqual('2012-02-02T1232');
+        });
+
+        it("should not add helpers for other attributes", function () {
+            var model = consistentModel();
+            model.color = 'blue';
+            expect(model.color).toEqual('blue');
+            expect(model.color_date).toBeUndefined();
+        });
+
+    });
+});
