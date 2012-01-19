9803c0b380192a4fe2a618f662b5843a67cb436e
Conrad Irwin <conrad.irwin@gmail.com>
2012-01-18 16:08:56 -0800

    Return _public in showable()

--- js/lib/showable.js
+++ js/lib/showable.js
@@ -7,14 +7,17 @@ lib.showable = function (_public, _protected) {
 
     _public.show = function () {
         _public.visible = true;
+        return _public;
     };
 
     _public.hide = function () {
         _public.visible = false;
+        return _public;
     };
 
     _public.toggle = function () {
-        _public.visible = !_public.visible;
+        _public.visible = arguments.length ? arguments[0] : !_public.visible;
+        return _public;
     };
 
     _public.onDestroy(_public.hide);
