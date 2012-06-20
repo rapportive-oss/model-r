0cb35a999d31533f61f8f8ba42071ecee906d9a0
Conrad Irwin <conrad.irwin@gmail.com>
2012-06-19 22:46:57 -0700

    Update ipad installer

--- js/lib/showable.js
+++ js/lib/showable.js
@@ -1,4 +1,4 @@
-lib.showable = function (_public, _protected) {
+lib.showable = function (_public, _protected, manager) {
 
     lib.model(_public, _protected, 'visible');
     lib.destroyable(_public, _protected);
@@ -21,6 +21,33 @@ lib.showable = function (_public, _protected) {
     };
 
     _public.onDestroy(_public.hide);
+
+    return _public;
+};
+
+// If you have a collection of mutually exclusive showables, this function will
+// ensure that the correct ones are hidden when the correct others are shown.
+// (aka. lib.thereShouldOnlyBeOne)
+lib.showable.manager = function () {
+
+    var _public = {}, visible = null;
+
+    _public.manage = function (showable) {
+        showable.wheneverEqual('visible', true, function () {
+            if (visible && visible !== showable) {
+                visible.hide();
+            }
+            visible = showable;
+        });
+
+        showable.wheneverEqual('visible', false, function () {
+            if (showable === visible) {
+                visible = null;
+            }
+        });
+    };
+
+    _(arguments).chain().flatten().each(_public.manage);
 
     return _public;
 };
