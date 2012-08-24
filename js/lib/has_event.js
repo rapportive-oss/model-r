355eca44eddbdf6382c77412da1b64d02a6e4a57
Conrad Irwin <conrad.irwin@gmail.com>
2012-08-24 00:22:25 -0700

    Remove remaining underscore extensions that are not used

--- js/lib/has_event.js
+++ js/lib/has_event.js
@@ -70,7 +70,7 @@ lib.hasEvent = function (_public, _protected, event_names) {
 
     // Returns true if anything is currently listening for the named event, false otherwise.
     _public.hasHandlers = _public.hasHandlers || function (event_name) {
-        return _(_protected.event_handlers[event_name]).andand().length > 0;
+        return (_protected.event_handlers[event_name] || []).length > 0;
     };
 
     // Allow either lib.hasEvent(_p, _p, ['a', 'b', 'c']),
--- js/vendor/underscore_extensions.js
+++ js/vendor/underscore_extensions.js
@@ -1,42 +1,8 @@
-/*jslint onevar: false, regexp: false */
-/*global setTimeout, clearTimeout */
+/*jslint onevar: false, regexp: false, nomen: false */
+/*global setTimeout, clearTimeout, _ */
 
 (function (_) {
     _.mixin({
-        // andand from ruby: returns an empty object if the input is falsey,
-        // otherwise returns the input (or calls the function with the input).
-        andand: function (obj, func) {
-            if (obj) {
-                return func ? func(obj) : obj;
-            }
-            return {};
-        },
-
-        // chomp() similar to ruby: Returns a new String with the given record
-        // separator removed from the end of input (if present). Defaults to
-        // removing spaces, newlines and carriage returns.
-        chomp: function (input, separator) {
-            if (!separator) {
-                separator = "[\r\n ]";
-            }
-            return input.replace(new RegExp(separator + '+$'), '');
-        },
-
-        // Returns a new string where runs of the same character that occur in
-        // this set are replaced by a single character.
-        squeeze: function (input, other_string) {
-            if (!other_string) {
-                throw "squeeze() without a specific string to squeeze, is not supported.";
-            }
-            return input.replace(new RegExp("(" + other_string + ")+", 'g'), other_string);
-        },
-
-        // Find the (asymetric) difference between the two arrays.
-        // _.difference([1,2,3], [2,3]) === _.without([1,2,3], 2, 3)
-        difference: function (ary1, ary2) {
-            return _.without.apply(_, [ary1].concat(ary2));
-        },
-
         // Turns the first letter of a string into uppercase.
         capitalize: function (str) {
             return str.charAt(0).toUpperCase() + str.slice(1);
@@ -64,57 +30,6 @@
                .replace(/([a-z0-9])([A-Z])/g, '$1_$2')
                .replace(/\-/g, '_')
                .toLowerCase();
-        },
-
-        // Escapes a string for inclusion into a regular expression.
-        // cf. http://simonwillison.net/2006/Jan/20/escape/
-        regexpEscape: function (str) {
-            return str.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, "\\$&");
-        },
-
-        // Returns a list of items such that the given function
-        // returns a unique value for each of them.
-        //
-        // If you know in advance that all duplicate values are
-        // adjacent to each other, you can pass isSorted to use
-        // a much faster algorithm.
-        //
-        // The fourth parameter is used as the  as with _.map
-        // when the provided function is called, while the
-        uniqBy: function (ary, func, isSorted, context) {
-            var seen = [],
-                uniq = [];
-            _.each(ary, function (el, i) {
-                var key = func.call(context, el, i, ary);
-                if (0 === i || (isSorted === true ? _.last(seen) !== key : !_.include(seen, key))) {
-                    seen.push(key);
-                    uniq.push(el);
-                }
-            });
-            return uniq;
-        },
-
-        // A function that, given an element, returns a property.
-        //   _([{1:2}, {1:3}]).map(_.plucker(1)) === [2, 3]
-        // (Though you could use _.pluck in that case).
-        //
-        plucker: function (key) {
-            return function (el) {
-                return el[key];
-            };
-        },
-
-        // Return the first truthy output of the iterator when
-        // passed over the array.
-        // (equivalent to .map().compact().first(), though more efficient).
-        firstMap: function (obj, iterator, context) {
-            var result;
-            _.any(obj, function (value, index, list) {
-                result = iterator.call(context, value, index, list);
-                return !!result;
-            });
-            return result;
         }
     });
-
 }(_));
